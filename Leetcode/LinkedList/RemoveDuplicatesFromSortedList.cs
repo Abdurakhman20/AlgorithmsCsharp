dfa3b615aad7ea1e57f8a98e5c897bf727e8fa67
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-17 23:23:59 +0300

    Solve algorithm LinkedList/RemoveDuplicatesFromSortedList

---
+++ Leetcode/LinkedList/RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,43 @@
+﻿namespace Leetcode.LinkedList
+{
+    public class RemoveDuplicatesFromSortedList
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null) 
+                return null;
+
+            if (head.next == null) 
+                return head;
+
+            ListNode current = head;
+
+            while (current.next != null)
+            {
+                if (current.val == current.next.val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+
+            }
+
+            return head;
+        }
+
+    }
+}
--- Leetcode/Root.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Leetcode
-{
-    public class Root
-    {
-
-    }
-}
