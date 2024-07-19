2a4988fe882877310cf3bcf43a05483bd60df56a
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-19 18:59:09 +0300

    Add BubbleSort algorithm

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,7 +6,13 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-           
+            int[] array = new int[] { 90, 9, 111, 11, 1, 0 };
+            Sort.BubbleSort(array);
+
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
         }
     } 
 }
---
+++ Algorithms/Sort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Sort
+    {
+        public static void BubbleSort(int[] array)
+        {
+            int tmp;
+
+            for (int i = 0; i < array.Length - 1; i++) 
+            {
+                for(int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if(array[j] > array[j + 1])
+                    {
+                        tmp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
