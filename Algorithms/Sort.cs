98511977dbae43f142b863cb9bab3e1cfb48a560
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-19 20:47:42 +0300

    Add SelectionSort algorithm

--- Algorithms/Sort.cs
+++ Algorithms/Sort.cs
@@ -42,5 +42,26 @@ namespace Algorithms
                 array[j + 1] = tmp;
             }
         }
+
+        public static void SelectionSort(int[] array)
+        {
+            
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int tmp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = tmp;
+            }
+        }
     }
 }
