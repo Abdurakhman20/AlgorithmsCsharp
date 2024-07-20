66f19022c8ca87e6739236b9e3b1cfdbe53fd682
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-20 21:49:10 +0300

    Fix Binary Search Tree

---
+++ DataStructures/BinarySearchTree.cs
@@ -0,0 +1,34 @@
+﻿namespace DataStructures
+{
+    public class BinarySearchTree
+    {
+        public TreeNode Root { get; set; } = null;
+
+        public void Insert(int key)
+        {
+            Root = InsertItem(Root, key);
+        }
+
+        private static TreeNode InsertItem(TreeNode node, int key)
+        {
+            TreeNode newNode = new TreeNode(key);
+
+            if(node == null)
+            {
+                node = newNode;
+                return node;
+            }
+
+            if(key < node.Key)
+            {
+                node.Left = InsertItem(node.Left, key);
+            }
+            else
+            {
+                node.Right = InsertItem(node.Right, key);
+            }
+
+            return node;
+        }
+    }
+}
--- DataStructures/TreeNode.cs
+++ DataStructures/TreeNode.cs
@@ -3,13 +3,11 @@
     public class TreeNode
     {
         public int Key {  get; set; }
-        public string Value { get; set; }
         public TreeNode Left { get; set; }
         public TreeNode Right { get; set; }
-        public TreeNode(int key, string value) 
+        public TreeNode(int key) 
         {
             this.Key = key;
-            this.Value = value;
         }
     }
 }
