2dd6667b6957cb3a1f67017abd0e74df9db9ceb9
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-20 13:49:28 +0300

    Add Tree Data Structure Implementation #1

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,10 +6,7 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-            Console.WriteLine(Search.BinarySearch(arr, 5));
-
+            
         }
     } 
 }
---
+++ DataStructures/TreeNode.cs
@@ -0,0 +1,15 @@
+﻿namespace DataStructures
+{
+    public class TreeNode
+    {
+        public int Key {  get; set; }
+        public string Value { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+        public TreeNode(int key, string value) 
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+}
