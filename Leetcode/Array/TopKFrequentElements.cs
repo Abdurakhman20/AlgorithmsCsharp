6d6775ab82ed34cde54eae483c2fb188fd7aa4c4
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-22 20:12:30 +0300

    Solve Top k frequent elements leetcode task

---
+++ Leetcode/Array/TopKFrequentElements.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Leetcode.Array
+{
+    public class TopKFrequentElements
+    {
+        public static int[] TopKFrequent(int[] nums, int k)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach(int num in nums)
+            {
+                if(!dict.ContainsKey(num))
+                {
+                    dict.Add(num, 0);
+                }
+
+                dict[num]++;
+            }
+
+            var result = dict.OrderByDescending(el => el.Value).Take(k).Select(el => el.Key).ToArray();
+
+            return result;
+        }
+    }
+}
