4b9eee93330d9574f3ec32069a511de811c59473
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-22 23:21:35 +0300

    Solve Product of array except self leetcode task

---
+++ Leetcode/Array/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Leetcode.Array
+{
+    public class ProductOfArrayExceptSelf
+    {
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            int N = nums.Length;
+            
+            int[] result = new int[N];
+            result[0] = 1;
+
+            for(int i = 1; i < N; i++)
+            {
+                result[i] = result[i - 1] * nums[i - 1];
+            }
+
+            int prefix = 1;
+
+            for(int i = N - 1; i >= 0; i--)
+            {
+                result[i] = result[i] * prefix;
+                prefix = prefix * nums[i];
+            }
+
+            return result;
+        }
+    }
+}
