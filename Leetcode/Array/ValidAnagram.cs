6b7878b1fd07ac2fac9358668e08764a78c69d29
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-22 14:56:14 +0300

    Solve valid anagram leetcode task

---
+++ Leetcode/Array/ValidAnagram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Leetcode.Array
+{
+    public class ValidAnagram
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            s = String.Concat(s.OrderBy(s => s));
+            t = String.Concat(t.OrderBy(s => s));
+
+            return s == t;
+
+        }
+    }
+}
