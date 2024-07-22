fb1acf32f1dc191a7bc44d5540a413d4151a5d37
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-22 15:01:20 +0300

    Add tests to valid anagram leetcode task

---
+++ Leetcode.Tests/Array/ValidAnagramTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using Leetcode.Array;
+
+namespace Leetcode.Tests.Array
+{
+    public class ValidAnagramTest
+    {
+        [Fact]
+        public void IsAnagram_ShouldReturnTrue()
+        {
+            string s = "anagram";
+            string t = "nagaram";
+
+            var result = ValidAnagram.IsAnagram(s, t);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsAnagram_ShouldReturnFalse()
+        {
+            string s = "rat";
+            string t = "car";
+
+            var result = ValidAnagram.IsAnagram(s, t);
+
+            Assert.False(result);
+        }
+    }
+}
