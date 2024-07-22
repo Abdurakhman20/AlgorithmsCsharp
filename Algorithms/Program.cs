462dc122c4ac5e945ec2d4c4aeb50f50c26769bc
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-22 14:10:57 +0300

    Solve contains duplicate leetcode task

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,19 +6,7 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            DataStructures.BinarySearchTree bst = new DataStructures.BinarySearchTree();
-
-            bst.Insert(20);
-            bst.Insert(15);
-            bst.Insert(14);
-            bst.Insert(16);
-            bst.Insert(30);
-            bst.Insert(35);
-            bst.Insert(28);
-            bst.Insert(32);
-            bst.Insert(40);
-
-            DataStructures.BinarySearchTree.PrintPostorder(bst.Root);
+            
         }
     } 
 }
---
+++ Leetcode/Array/ContainsDuplicate.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Leetcode.Array
+{
+    public class ContainsDuplicate
+    {
+        public bool CheckContainsDuplicate(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+
+            return set.Count != nums.Length;
+        }
+    }
+}
