fd850b45f4f3f9bc567bc1ba925478c3291be472
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-22 16:12:59 +0300

    Solve two sum leetcode task

---
+++ Leetcode/Array/TwoSum.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Leetcode.Array
+{
+    public class TwoSum
+    {
+        public static int[] TwoSumSolution(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(dict.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { dict[target - nums[i]], i};
+                }
+
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], i);
+                }
+            }
+
+            return null;
+        }
+    }
+}
