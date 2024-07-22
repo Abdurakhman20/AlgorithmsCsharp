a68a13b31d2c3dde1532ffd7cafc8218e2ca1347
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-22 20:30:21 +0300

    Add test to top k frequent elements leetcode task

---
+++ Leetcode.Tests/Array/TopKFrequentElementsTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Leetcode.Array;
+
+namespace Leetcode.Tests.Array
+{
+    public class TopKFrequentElementsTest
+    {
+        [Fact]
+        public void TopKFrequent_ShouldReturnOneAndTwo()
+        {
+            int[] nums = { 1, 1, 1, 2, 2, 3 };
+            int k = 2;
+
+            var result = TopKFrequentElements.TopKFrequent(nums, k);
+
+            var expectedResult = new int[] { 1, 2 };
+
+            Assert.Equal(expectedResult.Length, result.Length);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.Equal(expectedResult[i], result[i]);
+            }
+        }
+    }
+}
