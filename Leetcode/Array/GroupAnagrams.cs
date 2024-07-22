54c802d40689e7f2c42ba9ef947399e0a8b46f27
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-22 18:59:04 +0300

    Solve group anagrams leetcode task

---
+++ Leetcode/Array/GroupAnagrams.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Leetcode.Array
+{
+    public class GroupAnagrams
+    {
+        public static IList<IList<string>> GroupAnagramsSolution(string[] strs)
+        {
+            var dict = new Dictionary<string, IList<string>>();
+            foreach (string str in strs)
+            {
+                string strKey = String.Concat(str.OrderBy(s => s));
+                if(!dict.ContainsKey(strKey))
+                {
+                    dict[strKey] = new List<string>();
+                } 
+                
+                dict[strKey].Add(str);               
+            }
+
+            return dict.Values.ToList();
+        }
+    }
+}
