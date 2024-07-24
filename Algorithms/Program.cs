c56378c2b117d79708ea19def3dff308cec4dd30
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-24 20:36:01 +0300

    Solve valid palindrome leetcode task

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using Leetcode.Array;
+using Leetcode.TwoPointers;
 
 namespace Algorithms 
 {
@@ -7,8 +8,7 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            int[] arr = { 100, 4, 200, 1, 3, 2 };
-            var res = LongestConsecutiveSequence.LongestConsecutive(arr);
+            var res = ValidPalindrome.IsPalindrome("A man, a plan, a canal: Panama");
             Console.WriteLine(res);
         }
     } 
---
+++ Leetcode/TwoPointers/ValidPalindrome.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Leetcode.TwoPointers
+{
+    public class ValidPalindrome
+    {
+        public static bool IsPalindrome(string s)
+        {
+            s = Regex.Replace(s, @"[^a-zA-Z0-9]", "").ToLower();
+
+            int start = 0;
+            int end = s.Length - 1;
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(start <= end)
+                {
+                    if (s[start] != s[end])
+                    {
+                        return false;
+                    }
+                }
+                start += 1;
+                end -= 1;
+            }
+
+            return true;
+        }
+    }
+}
