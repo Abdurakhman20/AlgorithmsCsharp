9d27fa6ea477256e91041ba634c890f38b2996c5
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-24 21:12:47 +0300

    Add tests to valid palindrome leetcode task

---
+++ Leetcode.Tests/TwoPointers/ValidPalindromeTest.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using Leetcode.TwoPointers;
+
+namespace Leetcode.Tests.TwoPointers
+{
+    public class ValidPalindromeTest
+    {
+        [Fact]
+        public void IsPalindrome_ShouldReturnTrue()
+        {
+            // Arrange
+            string s = "A man, a plan, a canal: Panama";
+
+            // Act
+            bool result = ValidPalindrome.IsPalindrome(s);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsPalindrome_ShouldReturnFalse()
+        {
+            // Arrange
+            string s = "race a car";
+
+            // Act
+            bool result = ValidPalindrome.IsPalindrome(s);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
