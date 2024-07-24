8cbbd942c4c6f3a5c529c5836eedb250a37e7495
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-24 19:21:16 +0300

    Solve valid sudoku leetcode task

---
+++ Leetcode/Array/ValidSudoku.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Leetcode.Array
+{
+    public class ValidSudoku
+    {
+        public static bool IsValidSudoku(char[][] board)
+        {         
+            int state = 0;
+
+            bool IsNotValid(int bit)
+            {
+                if ((state >> bit & 1) > 0)
+                    return true; 
+
+                state |= 1 << bit;       
+                return false;
+            }
+
+            for (int i = 0; i < 9; i++)  
+            {
+                for (int j = 0; j < 9; j++)         
+                {            
+                    if (board[i][j] != '.') 
+                        if (IsNotValid(board[i][j] - '1'))   
+                            return false;
+                
+                    if (board[j][i] != '.')
+                        if (IsNotValid(board[j][i] - '1' + 9)) 
+                            return false;
+                 
+                    int y = j / 3 + (i / 3) * 3;     
+                    int x = j % 3 + (i % 3) * 3;      
+                    if (board[y][x] != '.')
+                        if (IsNotValid(board[y][x] - '1' + 18)) 
+                            return false;
+                }
+
+                state = 0;
+            }
+
+            return true;
+        }
+    }
+}
