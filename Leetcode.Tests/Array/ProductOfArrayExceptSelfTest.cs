306317f4a032ec90618f2294b93d739e616e2aa1
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-24 18:37:19 +0300

    Add test to product of array except self leetcode task #4

---
+++ Leetcode.Tests/Array/ProductOfArrayExceptSelfTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using Leetcode.Array;
+
+namespace Leetcode.Tests.Array
+{
+    public class ProductOfArrayExceptSelfTest
+    {
+        [Fact]
+        public void ProductExceptSelf_ShouldReturnProduct()
+        {
+            // Arrange
+            int[] nums = [ 1, 2, 3, 4 ];
+
+            // Act
+            int[] result = ProductOfArrayExceptSelf.ProductExceptSelf(nums);
+            int[] expextedResult = [ 24, 12, 8, 6 ];
+
+            // Assert
+            Assert.Equal(expextedResult.Length, result.Length);
+
+            for (int i = 0; i < result.Length; i++) 
+            {
+                Assert.Equal(expextedResult[i], result[i]);
+            }
+        }
+    }
+}
