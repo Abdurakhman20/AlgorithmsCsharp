ddc9b8abbcad1d75e7841ccb754239f572a6a6e3
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-25 23:03:01 +0300

    Solve Two Sum II - Input Array Is Sorted leetcode task

---
+++ Leetcode/TwoPointers/TwoSumInputArrayIsSorted.cs
@@ -0,0 +1,35 @@
+﻿namespace Leetcode.TwoPointers
+{
+    public class TwoSumInputArrayIsSorted
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int start = 0;
+            int end = numbers.Length - 1;
+            bool isFound = false;
+
+            while (start < end && !isFound)
+            {
+                int sum = numbers[start] + numbers[end];
+
+                if (sum == target)
+                {
+                    isFound = true;
+                    return new int[] { start + 1, end + 1 };
+                }
+
+                if (sum < target)
+                {
+                    start++;
+                }
+
+                else
+                {
+                    end--;
+                }
+            }
+
+            return null;
+        }
+    }
+}
